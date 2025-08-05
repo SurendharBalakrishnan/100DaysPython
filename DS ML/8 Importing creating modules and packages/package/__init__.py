8e1e079762b830a5a76f065bc1637810984b2012
SurendharBalakrishnan <surendharal65@gmail.com>
2025-08-05 23:32:22 +0530

    day 16  import modules and packages

---
+++ DS ML/8 Importing creating modules and packages/package/__init__.py
@@ -0,0 +1,9 @@
+'''
+__init__.py is a special Python file that indicates to the Python interpreter that the directory it is in should be treated as a package.
+It can be empty, or it can contain initialization code for the package. This file allows you
+
+
+__init__.py is a special file used in python to define a package 
+and initialize their namespaces
+'''
+
---
+++ DS ML/8 Importing creating modules and packages/test.py
@@ -0,0 +1,6 @@
+from package.math import *
+from package.subpackages.mult import multiply
+print(addition(5, 3))  # Output: 8
+print(subtraction(10, 4))  # Output: 6
+
+print(multiply(3, 4))  # Output: 12
